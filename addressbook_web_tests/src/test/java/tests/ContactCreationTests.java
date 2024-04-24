f9b3507d5633748e2696827ec2fe1f2c5fe06086
alexander-ruzanov <korsar274@mail.ru>
2024-04-24 20:26:08 +0300

    Реализована прочтение и загрузка информации из файла с тестовыми данными

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,36 +1,37 @@
 package tests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import common.CommonFunctions;
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-    public static List<ContactData> contactProvider() {
+    public static List<ContactData> contactProvider() throws IOException {
         var result = new ArrayList<ContactData>();
-        for (var firstname : List.of("", "contact firstname")){
-            for (var lastname : List.of("", "contact lastname")){
-                for (var address : List.of("", "contact address")){
-                    for (var home : List.of("", "contact home")) {
-                        for (var email : List.of("", "contact email")) {
-                            result.add(new ContactData().withFirstName(firstname).withLastName(lastname).withAddress(address));
-                        }
-                    }
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            result.add(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(i*10))
-                    .withLastName(CommonFunctions.randomString(i*10))
-                    .withAddress(CommonFunctions.randomString(i*10)));
-        }
+//        for (var firstname : List.of("", "contact firstname")){
+//            for (var lastname : List.of("", "contact lastname")){
+//                for (var address : List.of("", "contact address")){
+//                    for (var home : List.of("", "contact home")) {
+//                        for (var email : List.of("", "contact email")) {
+//                            result.add(new ContactData().withFirstName(firstname).withLastName(lastname).withAddress(address));
+//                        }
+//                    }
+//                }
+//            }
+//        }
+        ObjectMapper mapper = new ObjectMapper();
+        var value = mapper.readValue(new File("contacts.json"), new TypeReference<List<ContactData>>() {});
+        result.addAll(value);
         return result;
     }
     @Test
