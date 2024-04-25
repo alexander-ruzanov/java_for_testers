c7c821b8953da4c254660d9793d853f68fd9056a
alexander-ruzanov <korsar274@mail.ru>
2024-04-25 19:12:28 +0300

    Проверено сохранение в форматах YAML и XML

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import common.CommonFunctions;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
@@ -29,18 +30,18 @@ public class GroupCreationTests extends TestBase {
    //             }
     //        }
    //     }
-        var json = "";
-        try (var reader = new FileReader("groups.json");
-        var breader = new BufferedReader(reader)) {
-            var line = breader.readLine();
-            while (line != null) {
-                json = json + line;
-                line = breader.readLine();
-            }
-        }
-      //  var json = Files.readString(Paths.get("groups.json"));
-        ObjectMapper mapper = new ObjectMapper();
-        var value = mapper.readValue(json, new TypeReference<List<GroupData>>() {});
+//        var json = "";
+//        try (var reader = new FileReader("groups.json");
+//        var breader = new BufferedReader(reader)) {
+//            var line = breader.readLine();
+//            while (line != null) {
+//                json = json + line;
+//                line = breader.readLine();
+//            }
+//        }
+//        var json = Files.readString(Paths.get("groups.json"));
+        var mapper = new XmlMapper();
+        var value = mapper.readValue(new File("groups.xml"), new TypeReference<List<GroupData>>() {});
         result.addAll(value);
         return result;
     }
