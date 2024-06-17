055565d9cdb213706e46b608c97cc1d47d649893
alexander-ruzanov <korsar274@mail.ru>
2024-06-17 12:42:34 +0300

    Добавление в отчёт информации о шагах сценария

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -1,5 +1,6 @@
 package manager;
 
+import io.qameta.allure.Step;
 import model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -22,7 +23,7 @@ public class GroupHelper extends HelperBase {
         submitGroupModification();
         returnToGroupsPage();
     }
-
+@Step
     public void createGroup(GroupData group) {
         openGroupsPage();
         initGroupCreation();
@@ -31,6 +32,7 @@ public class GroupHelper extends HelperBase {
         returnToGroupsPage();
     }
 
+   @Step
     public void removeGroup(GroupData group) {
         openGroupsPage();
         selectGroup(group);
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.Allure;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,12 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
-        if (app.hbm().getGroupCount() == 0){
-            app.hbm().createGroup(new GroupData("", "group name", "group header", "group footer"));
-        }
+        Allure.step("Checking precondition", step -> {
+            if (app.hbm().getGroupCount() == 0){
+                app.hbm().createGroup(new GroupData("", "group name", "group header", "group footer"));
+            }
+        });
+
 
         var oldGroups = app.hbm().getGroupList();
         var rnd = new Random();
@@ -23,8 +27,10 @@ public class GroupRemovalTests extends TestBase {
         var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.remove(index);
+        Allure.step("Validating results", step -> {
+            Assertions.assertEquals(newGroups, expectedList);
+        });
 
-        Assertions.assertEquals(newGroups, expectedList);
 
     }
     @Test
